3c9cb268a5c917e4a161e560755752026c98cd45
Eugenes <smtrkw@mail.ru>
2023-08-06 16:15:41 +0300

    renamed names of functions and variables

--- publish_comic.py
+++ publish_comic.py
@@ -6,10 +6,17 @@ from urllib.parse import urlsplit
 import requests
 from dotenv import load_dotenv
 
-from functions_upload_images import saved_image
+
+def save_image(url, image_path, image_name):
+    response = requests.get(url)
+    response.raise_for_status()
+    Path(image_path).mkdir(parents=True, exist_ok=True)
+    file_path = Path() / image_path / image_name
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
 
-def fetch_xkcd():
+def fetch_xkcd_comics():
     image_path = Path() / 'images' / 'xkcd'
     latest_comic_url = "https://xkcd.com/info.0.json"
     response = requests.get(url=latest_comic_url)
@@ -25,7 +32,7 @@ def fetch_xkcd():
     comic_text = comic["alt"]
     image_link_path = urlsplit(image_url).path
     _, image_path_tail = os.path.split(image_link_path)
-    saved_image(
+    save_image(
         image_url,
         image_path,
         image_path_tail
@@ -67,8 +74,8 @@ def upload_to_vk_server(
         upload_result = response.json()
         server = upload_result['server']
         photo = upload_result['photo']
-        hash = upload_result['hash']
-    return server, photo, hash
+        upload_result_hash = upload_result['hash']
+    return server, photo, upload_result_hash
 
 
 def save_photo_to_vk_server(
@@ -77,14 +84,14 @@ def save_photo_to_vk_server(
         vk_api_version,
         server,
         photo,
-        hash
+        upload_result_hash
 ):
     url = "https://api.vk.com/method/photos.saveWallPhoto"
     params = {
         "group_id": vk_group_id,
         "server": server,
         "photo": photo,
-        "hash": hash,
+        "hash": upload_result_hash,
         "access_token": vk_access_token,
         "v": vk_api_version
     }
@@ -123,8 +130,8 @@ def main():
     vk_group_id = os.environ['VK_GROUP_ID']
     vk_access_token = os.environ['VK_ACCESS_TOKEN']
     vk_api_version = os.environ['VK_API_VERSION']
-    file_path, comic_text = fetch_xkcd()
-    server, photo, hash = upload_to_vk_server(
+    file_path, comic_text = fetch_xkcd_comics()
+    server, photo, upload_result_hash = upload_to_vk_server(
         vk_group_id,
         vk_access_token,
         vk_api_version,
@@ -136,7 +143,7 @@ def main():
         vk_api_version,
         server,
         photo,
-        hash
+        upload_result_hash
     )
     publish_photo_to_wall(
         vk_group_id,
