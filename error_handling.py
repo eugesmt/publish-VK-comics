06df6aaf5bc6c796604f38a1302c18f4831f19cc
Eugenes <smtrkw@mail.ru>
2023-08-08 23:52:33 +0300

    removed exception interception in error handlers from vk api

--- error_handling.py
+++ error_handling.py
@@ -2,18 +2,13 @@ import requests
 
 
 def handle_vk_api_response(response_result):
-    try:
-        if response_result['error']:
-            error_code = response_result['error']['error_code']
-            error_message = response_result['error']['error_msg']
-            raise requests.RequestException(f"{error_code} {error_message}")
-    except KeyError:
-        pass
+    if response_result.get('error'):
+        error_code = response_result['error']['error_code']
+        error_message = response_result['error']['error_msg']
+        raise requests.RequestException(f"{error_code} {error_message}")
 
 
 def handle_upload_vk_server_response(response_result):
-    try:
+    if response_result.get('photo'):
         if response_result['photo'] == '[]':
             raise requests.RequestException("Картинка не загружена")
-    except KeyError:
-        pass
